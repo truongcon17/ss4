55ed29da83526cad343ea0eb83303c4fa9b6c692
truongcon17 <aduvip1111111111@gmail.com>
2024-11-25 13:08:53 +0700

    Add files via upload

---
+++ session04-01.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	// khai bao bien 
+	int firstNumber;
+	// nhap gia tri cho bien firstNumber tu ban phim
+	printf("Moi ban nhap mot so nguyen bat ki:");
+	scanf("%d",&firstNumber);
+	// xu ly yeu cau bai toan 
+	if(firstNumber == 0){
+		printf("So 0 khong phai la so duong hay so am "); 
+	}
+	else{
+		if(firstNumber > 0){
+			printf("So ban vua nhap la so duong");
+		}
+		else{
+			printf("So ban vua nhap là so am"); 
+		} 
+		}
+	    return 0; 
+ }
+   
+ 
+
